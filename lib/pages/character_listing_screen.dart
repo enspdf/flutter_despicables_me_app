ed8285bb16dfaac7078d7b975409dfdbe3e12734
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-07-13 21:32:04 -0500

    despicables_me_app adjustments

--- lib/pages/character_listing_screen.dart
+++ lib/pages/character_listing_screen.dart
@@ -23,25 +23,30 @@ class _CharacterListingScreenState extends State<CharacterListingScreen> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 32, top: 8),
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(text: 'Despicable Me', style: AppTheme.display1),
-                  TextSpan(text: '\n'),
-                  TextSpan(text: 'Characters', style: AppTheme.display2),
-                ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 32, top: 8),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(text: 'Despicable Me', style: AppTheme.display1),
+                      TextSpan(text: '\n'),
+                      TextSpan(text: 'Characters', style: AppTheme.display2),
+                    ],
+                  ),
+                ),
               ),
-            ),
+              Expanded(
+                child: CharacterWidget(),
+              ),
+            ],
           ),
-          Expanded(
-            child: CharacterWidget(),
-          ),
-        ],
+        ),
       ),
     );
   }
